a5e495f813b648150c8ffd03744d35af89fd6417
Tommaso Tobini <tommaso.tobini@gmail.com>
2016-12-05 20:03:14 +0100

    Init commit

---
+++ eroi.sql
@@ -0,0 +1,13 @@
+-- ----------------------------
+-- Table structure for `eroi`
+-- ----------------------------
+CREATE TABLE `eroi` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of eroi
+-- ----------------------------
+INSERT INTO `eroi` VALUES ('1', 'casa');
